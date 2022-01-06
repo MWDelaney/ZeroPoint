e666b8fdd0449edc1dc49f535634850675e6d65b
MWDelaney <michael.delaney@gmail.com>
2022-01-06 13:15:56 -0500

    :zap: Compress Sass output; skip Sass partials

--- src/config/templateLanguages.js
+++ src/config/templateLanguages.js
@@ -5,16 +5,22 @@
 
 module.exports = {
   scss: function () {
+    let path = require("path");
     let sass = require("sass");
     let config = {
       outputFileExtension: "css",
       compile: function (contents, inputPath) {
+        let parsed = path.parse(inputPath);
+        if(parsed.name.startsWith("_")) {
+          return;
+        }
         let includesPaths = [this.config.dir.includes];
         return (data) => {
           let result = sass.renderSync({
             file: inputPath,
             includesPaths,
             data: contents,
+            outputStyle: "compressed",
           });
           return result.css.toString("utf8");
         };
