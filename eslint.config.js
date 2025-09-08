da802632756dfb1a9418a6786997244d5609ebf9
mwdelaney <michael@michaeldelaney.me>
2025-09-08 09:12:01 -0400

    Remove metagen plugin, replace with simplified meta partial; simplify lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ export default [
   js.configs.recommended,
   {
     languageOptions: {
-      ecmaVersion: 2024,
+      ecmaVersion: 'latest',
       sourceType: 'module',
       globals: {
         ...globals.browser,
@@ -13,57 +13,11 @@ export default [
       },
     },
     rules: {
-      // Console is allowed in development
       'no-console': 'off',
-
-      // Enforce modern JavaScript practices
-      'prefer-const': 'error',
-      'prefer-arrow-callback': 'error',
-      'prefer-template': 'error',
-      'prefer-destructuring': ['error', {
-        array: true,
-        object: true,
-      }, {
-        enforceForRenamedProperties: false,
-      }],
-
-      // Code style
-      'comma-dangle': ['error', {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'never',
-      }],
-      'semi': ['error', 'always'],
-      'quotes': ['error', 'single', { avoidEscape: true }],
-      'indent': ['error', 2, { SwitchCase: 1 }],
-
-      // Best practices
-      'eqeqeq': ['error', 'always'],
-      'no-var': 'error',
-      'no-unused-vars': ['error', {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      }],
-      'no-use-before-define': ['error', {
-        functions: false,
-        classes: true,
-        variables: true,
-      }],
-
-      // Modern ES features
-      'object-shorthand': 'error',
-      'arrow-spacing': 'error',
-      'template-curly-spacing': 'error',
-      'rest-spread-spacing': 'error',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
   },
   {
-    name: 'zeropoint/ignores',
-    ignores: [
-      'public/**/*',
-      'node_modules/**/*',
-    ],
+    ignores: ['public/**/*', 'node_modules/**/*'],
   },
 ];
--- src/config/plugins.js
+++ src/config/plugins.js
@@ -3,20 +3,10 @@
  * https://www.11ty.dev/docs/plugins/
 */
 
-import metagenPlugin from 'eleventy-plugin-metagen';
 import { eleventyImageTransformPlugin } from '@11ty/eleventy-img';
 // import reusableComponents from "eleventy-plugin-reusable-components";
 
 export default {
-  /**
-   * Metagen plugin
-   * https://github.com/tannerdolby/eleventy-plugin-metagen
-   */
-  async metagen (eleventyConfig) {
-    // Add plugin to eleventyConfig
-    eleventyConfig.addPlugin(metagenPlugin);
-  },
-
   /**
    * Eleventy Image plugin
    * https://www.11ty.dev/docs/plugins/image/
