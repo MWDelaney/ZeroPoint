00587fc2522dfd979c7753d367ab7f8d3b7f17bd
mwdelaney <michael@michaeldelaney.me>
2025-09-10 13:11:55 -0400

    Don't publish blogs from the future

--- content/pages/pages.11tydata.js
+++ content/pages/pages.11tydata.js
@@ -1,11 +1,20 @@
+/**
+ * Eleventy data file for pages - handles permalink generation
+ */
 export default {
   eleventyComputed: {
+    // Generate permalink URLs for pages
     permalink: (data) => {
+      // If the page has an explicit permalink, use it
       if (data.permalink) return data.permalink;
+
+      // If no permalink is provided, generate permalink from title using slugify filter
       if (data.title) {
         const slug = data.eleventy.env.filters.slugify(data.title);
         return `/${slug}/`;
       }
+
+      // If neither permalink nor title is provided, this page is not published
       return false;
     },
   },
--- content/posts/posts.11tydata.js
+++ content/posts/posts.11tydata.js
@@ -1,11 +1,26 @@
+/**
+ * Eleventy data file for posts - handles permalink generation
+ */
 export default {
   eleventyComputed: {
+    // Generate permalink URLs for blog posts
     permalink: (data) => {
+
+      // If the post date is in the future, do not publish
+      if (data.date && new Date(data.date) > new Date()) {
+        return false;
+      }
+
+      // If the post has an explicit permalink, use it
       if (data.permalink) return data.permalink;
+
+      // If no permalink is provided, generate permalink from title using slugify filter
       if (data.title) {
         const slug = data.eleventy.env.filters.slugify(data.title);
         return `/blog/${slug}/`;
       }
+
+      // If neither permalink nor title is provided, this post is not published
       return false;
     },
   },
